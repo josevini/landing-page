5330ee89ba91ed7083489e919d0b566e8989601b
josevini <ferreiravini468@gmail.com>
2022-04-01 13:50:37 -0300

    Update Section

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const SectionBase = styled.section`
     width: 100%;
     height: 450px;
+    display: flex;
     position: relative;
-    background-color: lightgrey;
 `
