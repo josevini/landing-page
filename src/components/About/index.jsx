672f67539fa3627f6d6fe4a49330d3af49f2351a
josevini <ferreiravini468@gmail.com>
2022-04-01 21:58:24 -0300

    Update About

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import service from '../../assets/service.svg'
 import { AboutSection } from './styles'
+import {
+    Content,
+    Title,
+    Description,
+    Image
+} from './styles'
 
 const About = ({ id }) => {
     return (
         <AboutSection id={id}>
-            <h1>About</h1>
+            <Image src={service} />
+            <Content>
+                <Title>Quem somos?</Title>
+                <Description>Somos uma agência que atua no ramo de desenvolvimento de softwares. Criamos websites e aplicativos com as melhores ferramentas do mercado.</Description>
+            </Content>
         </AboutSection>
     )
 }
--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,6 +1,42 @@
 import styled from 'styled-components'
-import { SectionBase } from '../Section/styles'
+import { SectionBase } from '../SectionBase/styles'
 
 export const AboutSection = styled(SectionBase)`
-    
+    background-color: #F2F2F2;
+    align-items: center;
+    justify-content: space-evenly;
+
+    @media (max-width: 768px) {
+        height: 500px;
+        flex-direction: column-reverse;
+        justify-content: center;
+    }
+`
+
+export const Content = styled.section`
+    text-align: left;
+    max-width: 600px;
+    padding: 0 2em;
+`
+
+export const Title = styled.h1`
+    font-size: 2.5em;
+    line-height: 1.3em;
+    margin-bottom: 0.6em;
+
+    @media (max-width: 768px) {
+        font-size: 1.8em;
+    }
+`
+
+export const Description = styled.p`
+    line-height: 1.5em;
+    margin-bottom: 0.9em;
+    @media (max-width: 768px) {
+        font-size: 0.9em;
+    }
+`
+
+export const Image = styled.img`
+    width: max(40vw, 360px);
 `
