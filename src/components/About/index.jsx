e73c2117b190efeb61dd125cfe87b77663d86f81
josevini <ferreiravini468@gmail.com>
2022-04-01 21:03:18 -0300

    Create About

---
+++ src/components/About/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AboutSection } from './styles'
+
+const About = ({ id }) => {
+    return (
+        <AboutSection id={id}>
+            <h1>About</h1>
+        </AboutSection>
+    )
+}
+
+export default About
---
+++ src/components/About/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { SectionBase } from '../Section/styles'
+
+export const AboutSection = styled(SectionBase)`
+    
+`
