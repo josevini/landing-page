385721e5b115e11d82f6994e652976318091197d
josevini <ferreiravini468@gmail.com>
2022-04-04 13:22:37 -0300

    Update GlobalStyles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,11 +4,15 @@ export default createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
         font-family: arial, sans-serif;
     }
 
     html {
+        box-sizing: border-box;
         scroll-behavior: smooth;
+    }
+
+    *, *::before, *::after {
+        box-sizing: inherit;
     }
 `
