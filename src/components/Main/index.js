448550f17b0c6ae780b0dcf45d08a1ce11216283
josevini <ferreiravini468@gmail.com>
2022-04-04 13:23:01 -0300

    Update Main files

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import pc from '../../assets/pc.svg'
+import { Container } from './styles'
 import {
-    Container,
     Content,
     Title,
     Description,
     Image
-} from './styles'
+} from '../SectionBase/styles'
 
 const Main = () => {
     return (
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -2,43 +2,8 @@ import styled from 'styled-components'
 import { SectionBase } from '../SectionBase/styles'
 
 export const Container = styled(SectionBase)`
-    height: 600px;
     align-items: center;
     justify-content: space-evenly;
     color: #fff;
-    background: linear-gradient(103deg, rgba(36, 40, 55, 1) 0%, rgba(53, 58, 74, 1) 100%);
-
-    @media (max-width: 768px) {
-        height: 500px;
-        flex-direction: column;
-        justify-content: center;
-    }
-`
-
-export const Content = styled.section`
-    text-align: left;
-    max-width: 600px;
-    padding: 0 2em;
-`
-
-export const Title = styled.h1`
-    font-size: 2.5em;
-    line-height: 1.3em;
-    margin-bottom: 0.6em;
-
-    @media (max-width: 768px) {
-        font-size: 1.8em;
-    }
-`
-
-export const Description = styled.p`
-    line-height: 1.5em;
-    margin-bottom: 0.9em;
-    @media (max-width: 768px) {
-        font-size: 0.9em;
-    }
-`
-
-export const Image = styled.img`
-    width: max(40vw, 360px);
+    background: linear-gradient(130deg, rgba(36, 40, 55, 1) 30%, rgba(53, 58, 74, 1) 60%, rgba(88, 97, 133, 1) 100%);
 `
