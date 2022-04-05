fc166045f285a680f4fb0f67ca561049f4c9d712
josevini <ferreiravini468@gmail.com>
2022-04-05 14:22:22 -0300

    Update SectionBase

--- src/components/SectionBase/styles.js
+++ src/components/SectionBase/styles.js
@@ -2,7 +2,42 @@ import styled from 'styled-components'
 
 export const SectionBase = styled.section`
     width: 100%;
-    height: 500px;
+    height: 600px;
     display: flex;
     position: relative;
+
+    @media (max-width: 768px) {
+        height: 500px;
+        flex-direction: column;
+        justify-content: center;
+    }
+`
+
+export const Content = styled.section`
+    max-width: 600px;
+    display: flex;
+    flex-direction: column;
+    padding: 0 2em;
+`
+
+export const Title = styled.h1`
+    font-size: 2.5em;
+    line-height: 1.3em;
+    margin-bottom: 0.6em;
+
+    @media (max-width: 768px) {
+        font-size: 1.8em;
+    }
+`
+
+export const Description = styled.p`
+    line-height: 1.5em;
+    margin-bottom: 0.9em;
+    @media (max-width: 768px) {
+        font-size: 0.9em;
+    }
+`
+
+export const Image = styled.img`
+    width: max(40vw, 360px);
 `
