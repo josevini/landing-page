0221bfe2b0409bdf1b4e31d93aefb719cbde6d00
josevini <ferreiravini468@gmail.com>
2022-04-05 19:07:13 -0300

    Update About

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,8 +1,22 @@
 import styled from 'styled-components'
-import { SectionBase } from '../SectionBase/styles'
+import {
+    MainSection as MainSectionBase,
+    ContentSection as ContentSectionBase,
+    Title as TitleBase,
+    Description as DescriptionBase,
+    Image as ImageBase
+} from '../SectionBase/styles'
 
-export const Container = styled(SectionBase)`
+export const Container = styled(MainSectionBase)`
     background-color: #F2F2F2;
     align-items: center;
     justify-content: space-evenly;
 `
+
+export const Content = styled(ContentSectionBase)``
+
+export const Title = styled(TitleBase)``
+
+export const Description = styled(DescriptionBase)``
+
+export const Image = styled(ImageBase)``
